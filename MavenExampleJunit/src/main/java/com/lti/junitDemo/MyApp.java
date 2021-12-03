47c75fe1fbdddfc944c77fe40904f2e93c678e4b
ChandanKumarDS <chandankumards1999@gmail.com>
2021-12-03 10:03:46 +0530

    first commit

---
+++ MavenExampleJunit/src/main/java/com/lti/junitDemo/MyApp.java
@@ -0,0 +1,9 @@
+package com.lti.junitDemo;
+
+public class MyApp {
+	public String show()
+	{
+		return "Hello Maven";
+	}
+
+}
---
+++ MavenExampleJunit/src/test/java/com/lti/junitdemo/MyAppTest2.java
@@ -0,0 +1,24 @@
+package com.lti.junitdemo;
+
+//import static org.junit.Assert.*;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import org.junit.Test;
+//
+//import com.lti.junitDemo.MyApp;
+//
+//public class MyAppTest2 {
+//
+////	@Test
+////	public void test() {
+////		fail("Not yet implemented");
+//	 @Test
+//	 public void testShow()
+//	{ 
+//		MyApp m= new MyApp();
+//		String msg=m.show();
+//		
+//	assertEquals("Hello Maven", msg);
+//	}
+//
+//}
